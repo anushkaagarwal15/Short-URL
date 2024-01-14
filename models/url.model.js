5d27450335dda163b4b09cd2de61901c1ede895d
anushkaagarwal15 <raanushka15@gmail.com>
2024-01-14 19:16:01 +0530

    change

---
+++ models/url.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ShortUrlSchema = new Schema({
+    url: {
+        type: String,
+        required: true,
+
+    },
+    shortId: {
+        type: String,
+        required: true
+    }
+})
+
+const ShortUrl = mongoose.model('shortUrl', ShortUrlSchema)
+
+
+model.exports = ShortUrl
